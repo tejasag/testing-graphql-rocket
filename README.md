e1f9b594131d1ef10b1c112dc8a03edb78737520
tejasag <tejasagarwalbly@gmail.com>
2021-03-04 16:12:39 +0530

    wee

---
+++ README.md
@@ -0,0 +1,22 @@
+# Testing GraphQL with Rocket in Rust
+
+This repository just contains some code I used to test GraphQL with Rocket. 
+You might see some weird and bad code practises in the code but that is just the code from the examples directory of juniper :)
+
+### Requirements
+- Rust and Cargo installed.
+- Rustup version 1.23.1 or above
+
+### Building from source
+
+- **Download or Clone the repository**
+```shell
+git clone https://github.com/tejasag/testing-graphql-rocket
+```
+
+- **Run the code**
+```shell
+cargo run
+```
+
+I am pretty sure that's it for now lmao
---
+++ src/main.rs
@@ -0,0 +1,47 @@
+#![feature(decl_macro, proc_macro_hygiene)]
+
+use juniper::{EmptyMutation, EmptySubscription, RootNode};
+use rocket::{response::content, State};
+
+mod schema;
+use schema::{Database, Query};
+
+type Schema = RootNode<'static, Query, EmptyMutation<Database>, EmptySubscription<Database>>;
+
+#[rocket::get("/")]
+fn graphiql() -> content::Html<String> {
+    juniper_rocket::graphiql_source("/graphql", None)
+}
+
+#[rocket::get("/graphql?<request>")]
+fn get_graphql_handler(
+    context: State<Database>,
+    request: juniper_rocket::GraphQLRequest,
+    schema: State<Schema>,
+) -> juniper_rocket::GraphQLResponse {
+    request.execute_sync(&schema, &context)
+}
+
+#[rocket::post("/graphql", data = "<request>")]
+fn post_graphql_handler(
+    context: State<Database>,
+    request: juniper_rocket::GraphQLRequest,
+    schema: State<Schema>,
+) -> juniper_rocket::GraphQLResponse {
+    request.execute_sync(&schema, &context)
+}
+
+fn main() {
+    rocket::ignite()
+        .manage(Database::new())
+        .manage(Schema::new(
+            Query,
+            EmptyMutation::<Database>::new(),
+            EmptySubscription::<Database>::new(),
+        ))
+        .mount(
+            "/",
+            rocket::routes![graphiql, get_graphql_handler, post_graphql_handler],
+        )
+        .launch();
+}
